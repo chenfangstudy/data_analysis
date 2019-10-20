d8039dfd323e8fed5b27f57af091431ca70fbf95
mikitstudy <55679254+mikitstudy@users.noreply.github.com>
2019-10-20 22:46:49 +0800

    Add files via upload

---
+++ homework-chapter-3/黄美芳+ch3实训二.py
@@ -0,0 +1,45 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rcParams['font.sans-serif'] = 'SimHei'
+plt.rcParams['axes.unicode_minus'] = False
+
+data = np.load('./data/pop.npz', allow_pickle=True)
+pdata = data['data'][::-1]
+name = data['feature_names']
+labelx = name[2:6]
+# 直方图
+p1 = plt.figure(figsize=(20, 40))#设置画布
+for i in range(20):
+    p1.add_subplot(5, 4, i+1)
+    plt.bar(name[2:6], pdata[i+2, 2:6], width=0.5)
+    plt.xlabel('类型')
+    plt.ylabel('人口(万)')
+    plt.title(pdata[i+2][0])
+
+plt.savefig('./pic/1996~2015年人口特征分析直线图')
+#饼图
+p2 = plt.figure(figsize=(20, 40))
+explode = [0.01, 0.01, 0.01, 0.01]
+for j in range(20):
+    p2.add_subplot(5, 4, j+1)
+    plt.pie(pdata[j+2, 2:6], explode=explode,
+            labels=name[2:6], autopct='%1.lf%%')
+    plt.title(pdata[j+2][0])
+plt.savefig('./pic/1996~2015年人口特征分析饼图')
+
+# 箱线图
+gdp = (list(pdata[2:, 2]), list(pdata[2:, 3]),
+       list(pdata[2:, 4]), list(pdata[2:, 5]))
+p3 = plt.figure(figsize=(20, 40))
+plt.boxplot(gdp, notch=True, labels=name[2:6], meanline=True)
+plt.title('1996~2015年人口特征分析')
+plt.xlabel('类型')
+plt.ylabel('人口(万)')
+plt.savefig('./pic/1996~2015年人口特征分析箱线图')
+
+
+plt.show()
