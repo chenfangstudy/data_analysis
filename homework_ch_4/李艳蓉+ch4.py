554d4fbde213bfe054f1483787b6d9d729f4f7f8
lianrongli <55649733+lianrongli@users.noreply.github.com>
2019-12-03 15:01:57 +0800

    Add files via upload

---
+++ homework_ch_4/李艳蓉+ch4.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+
+main = pd.read_csv('data\第4章\Training_Master.csv',sep=',',encoding="gbk")
+print("主表的维度为：",main.ndim)
+print("主表的形状为：",main.shape)
+print("主表的形状为：",main.memory_usage())
+print("主表的索引为：",main.index)
+print("主表的值为：",main.values)
+print("主表的列名为：",main.columns)
+print("主表的数据类型为：",main.dtypes)
+print("主表的元素个数为：",main.size)
+print("主表的描述统计为：",main.describe())
+def dropNullStd(data):
+    beforelen = data.shape[1]
+    colisNull = data.describe().loc['count'] == 0
+    for i in range(len(colisNull)):
+        if colisNull[i]:
+            data.drop(colisNull.index[i],axis=1,inplace=True)
+    
+    stdisZero = data.describe().loc['std'] == 0
+    for i in range(len(stdisZero)):
+        if stdisZero[i]:
+            data.drop(stdisZero.index[i],axis=1,inplace=True)
+    afterlen = data.shape[1]
+    print('剔除的列的数目为：',beforelen-afterlen)
+    print('剔出后数据的形状为：',data.shape)
+dropNullStd(main)
+
+#实训2
+
+pon = pd.read_csv('data\第4章\Training_LogInfo.csv',sep=',',encoding='gbk')
+pup = pd.read_csv('data\第4章\Training_Userupdate.csv',sep=',',encoding='gbk')
+
+pon['Listinginfo1'] = pd.to_datetime(pon['Listinginfo1'])
+pon['LogInfo3']= pd.to_datetime(pon['LogInfo3'])
+pup['ListingInfo1']= pd.to_datetime(pup['ListingInfo1'])
+pup['UserupdateInfo2']= pd.to_datetime(pup['UserupdateInfo2'])
+
+print(pon['Listinginfo1'].dtypes)
+print(pon['LogInfo3'].dtypes)
+print(pup['ListingInfo1'].dtypes)
+print(pup['UserupdateInfo2'].dtypes)
+
+def ymw(data):
+    year = [i.year for i in data]
+    month = [i.month for i in data]
+    week = [i.week for i in data]
+    print(year[:5])
+    print(month[:5])
+    print(week[:5])
+
+ymw(pon['Listinginfo1'])
+ymw(pon['LogInfo3'])
+ymw(pup['ListingInfo1'])
+ymw(pup['UserupdateInfo2'])
+
+
+def userTimedelta(bdata,rdata):
+    timedelta = pd.to_datetime(bdata) - pd.to_datetime(rdata)
+    print('减去后的数据:',timedelta[:5])
+    print('数据类型:',timedelta[:5])
+
+userTimedelta(pon['Listinginfo1'],pon['LogInfo3'])
+userTimedelta(pup['ListingInfo1'],pup['UserupdateInfo2'])
+
+#实训3
+ponidgp = pon[['Idx','Listinginfo1','LogInfo1','LogInfo2','LogInfo3']].groupby(by='Idx')
+pupidgp = pup[['Idx','ListingInfo1','UserupdateInfo1','UserupdateInfo2']].groupby(by='Idx')
+print("最早更新时间，最晚更新时间为:",ponidgp[['LogInfo3']].agg(['max','min']).head())
+print("最早登陆时间，最晚登陆时间为:",pupidgp[['UserupdateInfo2']].agg(['max','min']).head())
+print("信息更新次数:",ponidgp.size())
+print("登陆次数:",pupidgp.size())
+
+#实训4
+ponpivot = pd.pivot_table(pon,index='Idx')
+pupCross = pd.crosstab(index=pup['Idx'],columns='UserupdateInfo1')
+print(pupCross)
+
+pon = pd.read_csv('data\第4章\Training_LogInfo.csv',sep=',',encoding='gbk')
+pup = pd.read_csv('data\第4章\Training_Userupdate.csv',sep=',',encoding='gbk')
+
+pon['Listinginfo1'] = pd.to_datetime(pon['Listinginfo1'])
+pon['LogInfo3']= pd.to_datetime(pon['LogInfo3'])
+pup['ListingInfo1']= pd.to_datetime(pup['ListingInfo1'])
+pup['UserupdateInfo2']= pd.to_datetime(pup['UserupdateInfo2'])
+
+print(pon['Listinginfo1'].dtypes)
+print(pon['LogInfo3'].dtypes)
+print(pup['ListingInfo1'].dtypes)
+print(pup['UserupdateInfo2'].dtypes)
+
+def ymw(data):
+    year = [i.year for i in data]
+    month = [i.month for i in data]
+    week = [i.week for i in data]
+    print(year[:5])
+    print(month[:5])
+    print(week[:5])
+
+ymw(pon['Listinginfo1'])
+ymw(pon['LogInfo3'])
+ymw(pup['ListingInfo1'])
+ymw(pup['UserupdateInfo2'])
+
+
+def userTimedelta(bdata,rdata):
+    timedelta = pd.to_datetime(bdata) - pd.to_datetime(rdata)
+    print('减去后的数据:',timedelta[:5])
+    print('数据类型:',timedelta[:5])
+
+userTimedelta(pon['Listinginfo1'],pon['LogInfo3'])
+userTimedelta(pup['ListingInfo1'],pup['UserupdateInfo2'])
+
+#实训3
+ponidgp = pon[['Idx','Listinginfo1','LogInfo1','LogInfo2','LogInfo3']].groupby(by='Idx')
+pupidgp = pup[['Idx','ListingInfo1','UserupdateInfo1','UserupdateInfo2']].groupby(by='Idx')
+print("最早更新时间，最晚更新时间为:",ponidgp[['LogInfo3']].agg(['max','min']).head())
+print("最早登陆时间，最晚登陆时间为:",pupidgp[['UserupdateInfo2']].agg(['max','min']).head())
+print("信息更新次数:",ponidgp.size())
+print("登陆次数:",pupidgp.size())
+
+#实训4
+ponpivot = pd.pivot_table(pon,index='Idx')
+pupCross = pd.crosstab(index=pup['Idx'],columns='UserupdateInfo1')
+print(pupCross)
